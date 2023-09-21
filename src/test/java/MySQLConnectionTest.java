ce62de1c74565bd4baa0d3adffecbef7bc12c3fa
otso <hhgg0017@gmail.com>
2023-09-22 01:19:24 +0900

    fix : mapper 경로 다시 설정 후 정상 작동 DB에 post로 보낸 Data 정상 작동

--- src/test/java/MySQLConnectionTest.java
+++ src/test/java/MySQLConnectionTest.java
@@ -6,9 +6,9 @@ import java.sql.DriverManager;
 
 public class MySQLConnectionTest {
     private static final String DRIVER ="com.mysql.jdbc.Driver";
-    private static final String URL="jdbc:mysql://127.0.0.1:3306/testDB";
+    private static final String URL="jdbc:mysql://127.0.0.1:3308/testDB";
     private static final String USER ="root";
-    private static final String PASSWORD ="tjdgus123!@";
+    private static final String PASSWORD ="[root]";
 
     @Test
     public void testConnection() throws Exception{
